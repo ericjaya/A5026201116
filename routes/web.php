50a134a57a639d13826029172ad35459c0ec63f1
ericjaya <ericjayaziz@gmail.com>
2021-11-11 15:20:00 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//tambahkan disini untuk link route tugas 4 (Cara 2)
+
+Route::get('praktikum2', function () {
+    //yang disini tidak banyak proses/tidak banyak persiapan meload form
+    return view('htmlpraktikum2');
+});
+
+//jika ternyata kasusnya butuh ngeload banyak proses
+
+Route::get('isiannama',"ViewController@showForm") ;
+Route::post('greetings',"ViewController@resultGreetings");
